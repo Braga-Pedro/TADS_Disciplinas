79e238d9abeda36501a264fba1421195ae0b36bb
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2024-02-25 12:28:20 -0300

    Q01 dobrando o numero

---
+++ ARQUITETURA DE COMPUTADORAS/01 Atividade arq_comp/Q01.asm
@@ -0,0 +1,19 @@
+# template
+.text 
+main:	#lendo o número inteiro
+	addi $2, $0, 5
+	syscall
+	# pegando o numero e colocando no registrador 9
+	add $9, $0, $2
+	# multiplacador
+	addi $8, $0, 2
+	# dobrando o numero
+	mult $8, $9
+	# pegando multiplicação
+	mflo $12
+	# inserindo no registrador 4
+	add $4, $0, $12
+	# imprimindo o valor
+	addi $2, $0, 1
+	syscall
+	
