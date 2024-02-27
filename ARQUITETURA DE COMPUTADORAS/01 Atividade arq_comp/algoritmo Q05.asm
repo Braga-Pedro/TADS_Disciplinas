c4f716c3a53bd0ef2f091a8a6615c23901ee194a
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2024-02-27 15:30:11 -0300

    Create algoritmo Q05.asm

---
+++ ARQUITETURA DE COMPUTADORAS/01 Atividade arq_comp/algoritmo Q05.asm
@@ -0,0 +1,7 @@
+1. Leia o número inteiro fornecido pelo usuário.
+2. Inicialize uma variável soma como 0 para armazenar a soma dos dígitos.
+3. Enquanto o número não for zero:
+     3.1. Obtenha o último dígito do número (resto da divisão por 10).
+     3.2. Adicione o dígito à variável soma.
+     3.3. Atualize o número dividindo-o por 10 (para remover o último dígito).
+4. Imprima a soma dos dígitos.
