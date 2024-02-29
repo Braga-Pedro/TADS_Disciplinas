38aa1004b86568992d41e49b7d9f6bf9143b73e6
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2024-02-29 14:32:34 -0300

    Create q01.cpp

---
+++ ALGORITMO/lista02/q01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  // variaveis que recebem input
+  int parametro1, parametro2, soma;
+  // entrada 1 e entrada 2
+  std::cin >> parametro1 >> parametro2;
+  // soma dos parametros
+  soma = parametro1 + parametro2;
+  // saida
+  std::cout << "Hello World! " << soma << std::endl;
+}
