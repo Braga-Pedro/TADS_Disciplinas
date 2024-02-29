3537913715a7b1e51ed67d0580e271ff65113d3a
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2024-02-29 17:49:52 -0300

    soma natural exercitando

---
+++ POO/beecrowd-exercicios/natural-sum/NaturalSum.java
@@ -0,0 +1,14 @@
+/**
+ * Um número natural é um número inteiro não negativo (0, 1, 2, 3, 4, 5,...). Sua tarefa neste problema é calcular a soma dos números naturais que estão presentes em um determinado intervalo [ A , B ], inclusive.
+ * Por exemplo, a soma dos números naturais no intervalo [2, 5] é 14 = (2 + 3 + 4 + 5).
+ * 
+ * Entrada
+ * O caso de teste contém dois inteiros A e B (1 ≤ A ≤ B ≤ 10 9 ), representando o limite inferior e o superior respectivamente.
+ * 
+ * Saída
+ * Para cada teste, a saída consiste em uma linha contendo a soma dos números naturais no intervalo.
+ */
+public class NaturalSum {
+    // declarar 
+    
+}
