8cc30f1357aa367a972830f183ccdb89d111cd77
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2024-03-20 14:26:46 -0300

    Create Arrays.java

---
+++ POO/beecrowd-exercicios/Arrays.java
@@ -0,0 +1,20 @@
+/*
+Faça um programa que leia um vetor X[10]. Substitua a seguir, todos os valores nulos e negativos do vetor X por 1. Em seguida mostre o vetor X.
+
+Entrada
+A entrada contém 10 valores inteiros, podendo ser positivos ou negativos.
+
+Saída
+Para cada posição do vetor, escreva "X[i] = x", onde i é a posição do vetor e x é o valor armazenado naquela posição.
+*/
+import java.utils.Scanner;
+
+public class Arrays{
+  public static void main(String[] args){
+
+    // iniciando um vetor de 10 posições
+    int[] vetor = {9,-5,0,3,-4,8,0,4,0,7}
+    
+    System.out.println();
+  }
+}
