bcbae2a7213c263f740a07a489f4f0a4a0d0c1b8
Braga-Pedro <pedro.projetos.prog@gmail.com>
2024-07-17 14:38:39 -0300

    Feat: Criando classe author e seus atributos | falta seus métodos

--- POO/gerenciadorBiblioteca/src/Author.java
+++ POO/gerenciadorBiblioteca/src/Author.java
@@ -1,15 +1,24 @@
+import java.util.List;
+
 public class Author {
+    /**     ATRIBUTS
+     *    - `name` (String)
+     *    - `email` (String)
+     *    - `books` (List<Book>)
+     */
+    private String nome;
+    private String email;
+    private List<String> books;
 
-/**     ATRIBUTS
- *    - `name` (String)
- *    - `email` (String)
- *    - `books` (List<Book>)
- */
-
-/**     METHODS
- *    E os seguintes métodos:
- *    - `getName()`, `getEmail()`, `getBooks()`
- *    - `setName(String name)`, `setEmail(String email)`
- *    - `addBook(Book book)`: adiciona um livro à lista de livros do autor.
- * */
+    public Author(String nome, String email, List<String> books) {
+        this.nome = nome;
+        this.email = email;
+        this.books = books;
+    }
+    /**     METHODS
+     *    E os seguintes métodos:
+     *    - `getName()`, `getEmail()`, `getBooks()`
+     *    - `setName(String name)`, `setEmail(String email)`
+     *    - `addBook(Book book)`: adiciona um livro à lista de livros do autor.
+     * */
 }
--- POO/gerenciadorBiblioteca/src/Main.java
+++ POO/gerenciadorBiblioteca/src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
+
     public static void main(String[] args) {
+        Author author = new Author();
+        Book book = new Book(
+            "Viagem ao centro da terra",
+            "123Code",
+            3,
+            author
+        );
         System.out.printf("Hello and welcome!");
     }
 }
